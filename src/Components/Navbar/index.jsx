ac7f52c74a2e3f478841a407450c29930d0cc933
ginettec93 <ginettecarrizo@gmail.com>
2023-07-07 22:53:31 -0300

    se han corregido detalles esteticos en el navbar y en card

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+    <nav className='bg-white flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink to={`${isUserSignOut ? './sign-in' : '/'}`}>
